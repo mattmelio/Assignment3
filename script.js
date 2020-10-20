0287705c28c0da3ba38d0cf457c5ac495660d4a3
mattmelio <70667820+mattmelio@users.noreply.github.com>
2020-10-20 17:44:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+var model  = [
+    {
+      name: "Dr. Christoforou",
+      expertise : "Machine Learing, Artificial Intelligence; Brain-computer interfaces; neurally-informed predicrtive models",
+      profile : "https://scholar.google.com/citations?user=0PkTM-MAAAAJ&hl=en"
+    },
+    {
+      name: "Dr. Yadav",
+      expertise : "Mobile Computing, Computer Systems",
+      profile : "https://scholar.google.co.in/citations?user=OsCNwzEAAAAJ&hl=en"
+    },
+  ];
 
+
+var render_view = (view_id, model_index) => {
+  console.log("Rendering View");
+  var source = document.querySelector(view_id).innerHTML;
+  var template = Handlebars.compile(source);
+  var html = template(model[model_index]);
+
+  document.querySelector("#view_widget").innerHTML = html;
+}
