c5a144e4e3648f7359eb4555753599de45aa7912
mattmelio <70667820+mattmelio@users.noreply.github.com>
2020-10-23 00:38:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,122 @@
-var model  = [
-    {
-      name: "Java Quiz",
-      expertise : "Machine Learing, Artificial Intelligence; Brain-computer interfaces; neurally-informed predicrtive models",
-      profile : "https://scholar.google.com/citations?user=0PkTM-MAAAAJ&hl=en"
-    },
-    {
-      name: "HTML Quiz",
-      expertise : "Mobile Computing, Computer Systems",
-      profile : "https://scholar.google.co.in/citations?user=OsCNwzEAAAAJ&hl=en"
-    },
-  ];
+var questions = [{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+},{
+    "question": "",
+    "option1":"",
+    "option2":"",
+    "option3":"",
+    "answer":""
+}]
 
-
-var render_view = (view_id, model_index) => {
-  console.log("Rendering View");
-  var source = document.querySelector(view_id).innerHTML;
-  var template = Handlebars.compile(source);
-  var html = template(model[model_index]);
-
-  document.querySelector("#view_widget").innerHTML = html;
-}
