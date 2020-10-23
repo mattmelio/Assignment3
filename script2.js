836d53e4342c5b172c8fbdbc9cac764b15cb1cf8
mattmelio <70667820+mattmelio@users.noreply.github.com>
2020-10-23 17:50:09 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -45,3 +45,13 @@ function loadNextQuestion () {
   loadQuestion(currentQuestion);
 }
  loadQuestion(currentQuestion);
+
+var render_view = (view_id, model_index) => {
+  console.log("Rendering View");
+  var source = document.querySelector(view_id).innerHTML;
+  var template = Handlebars.compile(source);
+  var html = template(model[model_index]);
+
+  document.querySelector("#view_widget").innerHTML = html;
+}
+
