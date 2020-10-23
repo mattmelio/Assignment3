b5a5cf52b3971ca6ec75bdc840c3e0a9db5c25b7
mattmelio <70667820+mattmelio@users.noreply.github.com>
2020-10-23 01:55:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,123 @@
 var questions = [{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "Which statement is true regarding an object?",
+    "option1":"An object is what classes instantiated are from",
+    "option2":"An object is an instance of a class",
+    "option3":"An object is a reference to an attribute",
+    "answer":"2"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "Object-oriented inheritance models the",
+    "option1":" “is a kind of” relationship",
+    "option2":"“want to be” relationship",
+    "option3":"inheritance does not describe any kind of relationship between classes",
+    "answer":"1"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "The wrapping up of data and functions into a single unit is called",
+    "option1":"Encapsulation",
+    "option2":"Message passing",
+    "option3":"Polymorphism",
+    "answer":"1"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": " A package is a collection of",
+    "option1":"Editing tools",
+    "option2":"Classes",
+    "option3":"Classes and interfaces",
+    "answer":"3"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "Basic Java language functions are stored in which of the following java package?",
+    "option1":"java.lang",
+    "option2":"java.util",
+    "option3":"java.util",
+    "answer":"1"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "How restrictive is the default accessibility compared to public, protected, and private accessibility?",
+    "option1":"More restrictive than public, but less restrictive than protected",
+    "option2":"More restrictive than protected, but less restrictive than private",
+    "option3":"More restrictive than private",
+    "answer":"2"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": " In object-oriented programming, new classes can be defined by extending existing classes. This is an example of:",
+    "option1":"Encapsulation",
+    "option2":"Inheritance",
+    "option3":"Interface",
+    "answer":"2"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "The default value of a static integer variable of a class in Java is,",
+    "option1":"1",
+    "option2":"3",
+    "option3":"0",
+    "answer":"3"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "To prevent any method from overriding, we declare the method as,",
+    "option1":"abstract",
+    "option2":"final",
+    "option3":"static",
+    "answer":"2"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "Which one of the following is not true?",
+    "option1":"A class must be qualified as ‘abstract’ class, if it contains one abstract method.",
+    "option2":"A class containing abstract methods is called an abstract class.",
+    "option3":" An abstract class cannot have non-abstract methods.",
+    "answer":"3"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "The fields in an interface are implicitly specified as,",
+    "option1":"both static and final",
+    "option2":"private",
+    "option3":"protected",
+    "answer":"1"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "Which of the following variable declaration would NOT compile in a java program?",
+    "option1":"int var;",
+    "option2":"int 1_var;",
+    "option3":"All of the above",
+    "answer":"2"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "The java run time system automatically calls this method while garbage collection.",
+    "option1":"finalize()",
+    "option2":"finalizer() ",
+    "option3":"finalized()",
+    "answer":"1"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "A constructor",
+    "option1":"Is used to create objects.",
+    "option2":"May be declared private",
+    "option3":"All of the above",
+    "answer":"1"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "Identify, from among the following, the incorrect variable name(s).",
+    "option1":"$reallyBigNumber",
+    "option2":"CurrentWeatherStateofplanet",
+    "option3":"2ndName",
+    "answer":"3"
 },{
-    "question": "",
+    "question": "An applet cannot be viewed using",
     "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "option2":"Applet viewer tool which comes, with the Java Development Kit.",
+    "option3":"Netscape navigator",
+    "answer":"2"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "What would the argument passing method be which is used by the above Program – III?",
+    "option1":"Call by byte code",
+    "option2":"Call by value",
+    "option3":"Call by compiler",
+    "answer":"2"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "Members of a class specified as ……………….. are accessible only to methods of that class.",
+    "option1":"public",
+    "option2":"final",
+    "option3":"private",
+    "answer":"3"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "In Java, a character constant’s value is its integer value in the ………………………character set.",
+    "option1":"EBCDIC",
+    "option2":"BCD",
+    "option3":"Unicode",
+    "answer":"3"
 },{
-    "question": "",
-    "option1":"",
-    "option2":"",
-    "option3":"",
-    "answer":""
+    "question": "In a class definition, the special method provided to be called to create an instance of that class is known as a/an",
+    "option1":"Constructor",
+    "option2":"Object",
+    "option3":"Interpreter",
+    "answer":"1"
 }]
 
 
