1e74a6d3b26d1d211a8a65515f0553ea097889d3
mattmelio <70667820+mattmelio@users.noreply.github.com>
2020-10-23 20:04:12 -0400

    Update quiz-script.js

--- quiz-script.js
+++ quiz-script.js
@@ -88,6 +88,15 @@ function loadNextQuestion () {
     },
   ];
 
+var render_view = (view_id, model_index) => {
+  console.log("Rendering View");
+  var source = document.querySelector(view_id).innerHTML;
+  var template = Handlebars.compile(source);
+  var html = template(model[model_index]);
+
+  document.querySelector("#view_widget").innerHTML = html;
+}
+
 
 
 
