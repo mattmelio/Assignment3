94b46dcb4b784e232c0cbcf52424d196395e0d35
mattmelio <70667820+mattmelio@users.noreply.github.com>
2020-10-30 13:34:14 -0400

    Update quiz-script.js

--- quiz-script.js
+++ quiz-script.js
@@ -10,14 +10,12 @@ var opt3= document.getElementById('opt3');
 var nextButton= document.getElementById('nextButton');
 var resultCont= document.getElementById('result');
 
-var model  = [
-    {
-  function loadQuestion (questionIndex) {
+function loadQuestion (questionIndex) {
   var q= questions[questionIndex];
   questionEL.textContent= (questionIndex +1)+ '.'+q.question;
   opt1.textContent= q.option1;
   opt2.textContent= q.option2;
-  opt3.textContent= q.option3; 
+  opt3.textContent= q.option3;
 };
 
 function loadNextQuestion () {
@@ -48,54 +46,6 @@ function loadNextQuestion () {
 }
  loadQuestion(currentQuestion);
 
-    },
-    {
-     funtion loadQuestion (questionIndex) {
-  var q= questions2[questionIndex];
-  questionEL.textContent= (questionIndex +1)+ '.'+q.question;
-  opt1.textContent= q.option1;
-  opt2.textContent= q.option2;
-  opt3.textContent= q.option3;
-};
-
-function loadNextQuestion () {
-  var selectedOption = document.querySelector('input[type=radio]:checked');
-  if(!selectedOption){
-    alert('Please select your answer!');
-    return;
-  }
-  
-  var answer= selectedOption.value;
-  if(questions2[currentQuestion].answer == answer){
-    score+=5;
-  }
-  selectedOption.check= false;
-  currentQuestion++;
-  
-  if(currentQuestion= totQuestions- 1){
-    nextButton.textContent='finish';
-  }
-  
-  if(currentQuestion= totQuestions){
-    container.style.display= 'none';
-    resultCont.style.display= '';
-    resultCont.textContent= 'Your Score is:' + score;
-    return;
-  }
-  loadQuestion(currentQuestion);
-}
- loadQuestion(currentQuestion);
-    },
-  ];
-
-var render_view = (view_id, model_index) => {
-  console.log("Rendering View");
-  var source = document.querySelector(view_id).innerHTML;
-  var template = Handlebars.compile(source);
-  var html = template(model[model_index]);
-
-  document.querySelector("#view_widget").innerHTML = html;
-}
 
 
 
